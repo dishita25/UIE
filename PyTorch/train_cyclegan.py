188995e3facd8a8fb07b98cb721b6aaddd2ba817
MSarayu512 <sarayu.nasa@gmail.com>
2025-09-19 00:50:44 +0530

    100 epochs

--- PyTorch/train_cyclegan.py
+++ PyTorch/train_cyclegan.py
@@ -137,7 +137,7 @@ def get_config():
     parser.add_argument("--lr_g", type=float, default=0.0002, help="Generator learning rate")
     parser.add_argument("--lr_d", type=float, default=0.0002, help="Discriminator learning rate") 
     parser.add_argument("--beta1", type=float, default=0.5, help="Beta1 for Adam optimizer")
-    parser.add_argument("--niter", type=int, default=10, help="Number of iterations")
+    parser.add_argument("--niter", type=int, default=100, help="Number of iterations")
     parser.add_argument("--batch_size", type=int, default=32, help="Batch size")
     parser.add_argument("--val_batch_size", type=int, default=32, help="Validation batch size")
     
